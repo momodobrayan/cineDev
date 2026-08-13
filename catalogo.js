6c8cd0582f0f8c66cf9cce4952f88818ad77b8d9
samira <samira.vogivoda@escola.pr.gov.br>
2026-08-13 08:59:25 -0300

    ok

--- catalogo.js
+++ catalogo.js
@@ -1,20 +1,27 @@
-export default class catalogo {
-    #listaDeMidias;
+export default class catalogo { 
+    #listaDemidias
     constructor() {
-        this.#listaDeMidias = [];
-    }
-    adicionar(midia) {
-        this.#listaDeMidias.push(midia);
-        this.renderizar();
-    }
-}
-export default class Catalogo {
-}
-renderizar() {
-    const container = document.getElementById("containerCatalogo");
-    container.innerHTML = "";
-    this.#listaDeMidia.forEach
-        
-    });
-    });
-}
+         this.#listaDemidias = [];
+    } 
+     adicionar (midia) { 
+      this.#listaDemidias.push(midia);
+      this.renderizar();
+
+
+     }  
+  renderizar() {
+  const contrainer = document.getElementById("containerCatalogo") 
+  comtainer. innerHTML = "";
+  this.#listaDemidias.forEach(midia => { 
+
+
+       contrainer.innerHTML += midia.obterDados
+
+  });
+ }
+      static filtrarPorGeneri(catalogoInstanciado, generoAlvo) {
+         return catalogoInstanciado.#listadeMidias.filter(
+             m => m.genero.toLowerCase() === generoAI
+         )
+        }
+    
