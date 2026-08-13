f1047129c11579f4ca836563e17f14d94c9775d5
samira <samira.vogivoda@escola.pr.gov.br>
2026-08-13 07:46:15 -0300

    ok

---
+++ catalogo.js
@@ -0,0 +1,20 @@
+export default class catalogo {
+    #listaDeMidias;
+    constructor() {
+        this.#listaDeMidias = [];
+    }
+    adicionar(midia) {
+        this.#listaDeMidias.push(midia);
+        this.renderizar();
+    }
+}
+export default class Catalogo {
+}
+renderizar() {
+    const container = document.getElementById("containerCatalogo");
+    container.innerHTML = "";
+    this.#listaDeMidia.forEach
+        
+    });
+    });
+}
